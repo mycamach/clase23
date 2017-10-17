74c47fe9b5b38ce35c1f3861aefcf719355802a7
mycamach <manoli.camacho.a@gmail.com>
2017-10-16 21:57:26 -0300

    Template bootstrap, images(some) and css added

---
+++ config/initializers/assets.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
+Rails.application.config.assets.precompile += %w[**/*.jpg **/*.png *.png *.jpg]
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Actividad 023 - Integración de una plantilla
+
+- Para poder realizar este actividad debes haber realizado los cursos previos junto con los videos online correspondientes a la experiencia 12.
+
+- Los archivos necesarios para la realización de esta actividad los puedes encontrar [este repositorio de Github.](https://github.com/DesafioLatam/E12CP1A1)
+
+## Ejercicio 1:
+
+- Descargar la plantilla desde el repositorio de GitHub.
+
+- Crear un nuevo proyecto en Rails.
+
+- Inicializar Git.
+
+- Implementar la plantilla en el proyecto.
+
+- Se evaluará:
+
+    - Cargado de archivos CSS.
+    
+    - Cargado de archivos JS.
+
+    - Cargado de imágenes.
+    
+    - Implementación de tipografías.
+
+- El proyecto debe ser subido a un repositorio en tu cuenta de GitHub.
